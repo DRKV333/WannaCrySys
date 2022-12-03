4e167512bf63a15267c6db15c7a6138362c97162
Gergely Gaál <gaal.gery99@t-online.hu>
2022-12-03 13:20:29 +0100

    Validation fix

--- Backend/CaffShop.Shared/DTOs/UserForUpdate.cs
+++ Backend/CaffShop.Shared/DTOs/UserForUpdate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,9 @@ namespace CaffShop.Shared.DTOs
     [MinLength(8, ErrorMessage = "Minimum 8 characters are required")]
     [MaxLength(32, ErrorMessage = "Maximum 32 characters are allowed")]
     public string Name { get; set; }
-    public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
+    [AllowNull]
+    public string? Password { get; set; }
+    [AllowNull]
+    public string? ConfirmPassword { get; set; }
   }
 }
